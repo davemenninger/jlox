8c32ccdb09d57b639981c175b2f6a4ec9ece2243
Dave Menninger <dmenninger@justworks.com>
2023-06-29 14:53:01 -0400

    visitor pattern for our AST

--- src/com/craftinginterpreters/lox/Expr.java
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -3,12 +3,23 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
+ interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
-    }
+  }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+    return visitor.visitBinaryExpr(this);
+  }
 
     final Expr left;
     final Token operator;
@@ -17,14 +28,24 @@ abstract class Expr {
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
-    }
+  }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+    return visitor.visitGroupingExpr(this);
+  }
 
     final Expr expression;
   }
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
-    }
+  }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+    return visitor.visitLiteralExpr(this);
+  }
 
     final Object value;
   }
@@ -32,9 +53,16 @@ abstract class Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
-    }
+  }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+    return visitor.visitUnaryExpr(this);
+  }
 
     final Token operator;
     final Expr right;
   }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
